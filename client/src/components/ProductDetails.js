aa6a0de3464556a038a913c8723d98c6a4352a57
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-01-19 16:34:01 -0800

    implemented reusable DeleteButton component on product details page

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,23 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { navigate } from "@reach/router";
 
+// components
+import DeleteButton from "./DeleteButton";
+
 const ProductDetails = (props) => {
-    const { id, deleteProduct } = props;
-
-    const [product, setProduct] = useState({});
-
-    useEffect(() => {
-        axios
-            .get(`http://localhost:8000/api/products/${id}`)
-            .then((res) => {
-                console.log(res.data);
-                setProduct(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
+    const { product } = props;
 
     return (
         <div className="card mx-5 my-2">
@@ -25,15 +13,10 @@ const ProductDetails = (props) => {
                 <h3 className="card-title">{product.title}</h3>
                 <h4 className="card-subtitle text-muted">$ {product.price}</h4>
                 <p className="card-text">{product.description}</p>
-                <button
-                    className="btn btn-outline-danger mx-5"
-                    onClick={(e) => {
-                        deleteProduct(product._id);
-                        navigate("/");
-                    }}
-                >
-                    Delete
-                </button>
+                <DeleteButton
+                    productID={product._id}
+                    successAction={() => navigate("/")}
+                />
             </div>
         </div>
     );
--- client/src/views/Detail.js
+++ client/src/views/Detail.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 // components
 import ProductDetails from "../components/ProductDetails";
 
-const Home = (props) => {
-    const { id, deleteProduct } = props;
+const Detail = (props) => {
+    const { id } = props;
+
+    const [product, setProduct] = useState();
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8000/api/products/${id}`)
+            .then((res) => {
+                console.log(res.data);
+                setProduct(res.data);
+                setLoaded(true);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
     return (
         <div>
             <h1 className="text-center">Product Details</h1>
-            <ProductDetails
-                id={id}
-                deleteProduct={deleteProduct}
-            />
+            {loaded && <ProductDetails product={product} />}
         </div>
     );
 };
 
-export default Home;
+export default Detail;
